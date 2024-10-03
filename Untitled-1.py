e40862ce9e7e9c06d41ba95313390d2acb9fb02d
ceeoo1 <wavsousa@gmail.com>
2024-10-03 00:31:47 -0300

    CHAT TESTE

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,32 @@
-print("ola krl")
+import os
+
+mensagens = [] #type: ignore
+
+nome = input("nome: ")
+
+# limpando terminal do windows (cls)
+
+while True: 
+    os.system('cls')
+    
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+
+    print("________")
+
+    # obter texto do chat
+
+    texto = input("mensagem: ")
+    if texto == "fim":
+        break
+    
+    # adicionando mensagens na lista
+
+    mensagens.append ({
+        "nome": nome,
+        "texto": texto
+    })
+    
+   
+        
